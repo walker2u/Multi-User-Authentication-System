45338abc64c3d07c7097ae71fd115b1589c2216a
walker2u <kumar99prasad@gmail.com>
2024-03-16 00:40:59 +0530

    made dashboard for admin and agent

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,7 +29,17 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+        $url = '';
+        if($request->user()->role === 'admin'){
+            $url = 'admin/dashboard';
+        }
+        else if($request->user()->role === 'agent'){
+            $url = 'agent/dashboard';
+        }else{
+            $url = '/dashboard';
+        }
+
+        return redirect()->intended($url);
     }
 
     /**
